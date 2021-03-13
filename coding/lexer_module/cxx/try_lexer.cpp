e16c5fc00230ead9d500eee0da09079c8b6a7e57
bxtkezhan <bxtkezhan@qq.com>
2021-03-13 23:55:23 +0800

    update

--- coding/lexer_module/cxx/try_lexer.cpp
+++ coding/lexer_module/cxx/try_lexer.cpp
@@ -12,28 +12,28 @@ int main() {
 	printf("--------------------------------\n");
 
     auto lexer = bklexer::Lexer();
-	lexer.add_rule("\\d+\\.\\d*", "FLOAT");
-	lexer.add_rule("\\d+", "INT");
-	lexer.add_rule("\\w+", "NAME");
-	lexer.add_rule("\\+", "PLUS");
-	lexer.add_rule("\\-", "MINUS");
-	lexer.add_rule("\\*", "MUL");
-	lexer.add_rule("/", "DIV");
-	lexer.add_rule("=", "ASSIGN");
-	lexer.add_rule("#[^\\r\\n]*", "COMMENT");
-	lexer.add_ignores("[ \\f\\t]+");
+    lexer.add_rule("\\d+\\.\\d*", "FLOAT");
+    lexer.add_rule("\\d+", "INT");
+    lexer.add_rule("\\w+", "NAME");
+    lexer.add_rule("\\+", "PLUS");
+    lexer.add_rule("\\-", "MINUS");
+    lexer.add_rule("\\*", "MUL");
+    lexer.add_rule("/", "DIV");
+    lexer.add_rule("=", "ASSIGN");
+    lexer.add_rule("#[^\\r\\n]*", "COMMENT");
+    lexer.add_ignores("[ \\f\\t]+");
 
-	lexer.build(code);
-	while (true) {
-		auto token = lexer.next_token();
-		if (token.ttype != TOKEN_TYPE_EOF) {
-			printf("%s\t%s\tt%d\t%d\t%d,%d\n",
-			token.name.c_str(), bklexer::quote(token.source).c_str(), token.ttype, token.position, token.row, token.col);
-		}
-		if (token.ttype == TOKEN_TYPE_EOF || token.ttype == TOKEN_TYPE_ERROR) {
-			break;
-		}
-	}
+    lexer.build(code);
+    while (true) {
+        auto token = lexer.next_token();
+        if (token.ttype != TOKEN_TYPE_EOF) {
+            printf("%s\t%s\tt%d\t%d\t%d,%d\n",
+            token.name.c_str(), bklexer::quote(token.source).c_str(), token.ttype, token.position, token.row, token.col);
+        }
+        if (token.ttype == TOKEN_TYPE_EOF || token.ttype == TOKEN_TYPE_ERROR) {
+            break;
+        }
+    }
 
     std::locale::global(old);
     return 0;
